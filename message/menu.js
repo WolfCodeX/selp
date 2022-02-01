e1d6093a47f4f4b001fee0e85fedf6d4a8a1968c
WolfCodeX <muhammaddesta2022@gmail.com>
2022-02-01 13:50:58 +0700

    selp

---
+++ message/menu.js
@@ -0,0 +1,206 @@
+exports.menu = (prefix) => {
+  let a = '*𝕱⃝🦢*' 
+  let b = '```'
+  let c = '⬡'
+  let d = '⬢'
+  let e = '⬣'
+  let o = 0
+  let G1 = '·‧໋݊  ⃝⃕🦢'
+  let G2 = '·‧໋݊  ⃝⃕🐤'
+  let G3 = '·‧໋݊  ⃝⃕🐸'
+  return ` 🇲🇨 *: 𝗗𝗮𝗳𝘁𝗮𝗿 𝗙𝗶𝘁𝘂𝗿 𝗬𝗮𝗻𝗴 𝗔𝗱𝗮 𝗗𝗶 𝗕𝗼𝘁 𝗜𝗻𝗶*
+🇬🇧 *: 𝗟𝗶𝘀𝘁 𝗢𝗳 𝗙𝗲𝗮𝘁𝘂𝗿𝗲 𝗕𝗼𝘁 𝗜𝗻 𝗛𝗲𝗿𝗲*
+🇯🇵 *: このボットの機能のリスト*
+
+*[ PROFILE ]*
+›❥ Library : Baileys-Md
+›❥ Version 1.1.0
+›❥ Author : XCodeTeam
+
+    ${b}🌹【 OWNER MENU 】🌹${b}
+${G1} ${prefix}broadcast
+${G1} ${prefix}setprefix
+${G1} ${prefix}public
+${G1} ${prefix}self
+${G1} ${prefix}addcmd
+${G1} ${prefix}delcmd
+${G1} ${prefix}setfake
+${G1} ${prefix}setfakeimage
+${G1} ${prefix}setthumb
+${G1} ${prefix}leaveall
+${G1} ${prefix}leavegroup
+${G1} ${prefix}autoread
+${G1} ${prefix}deletepc
+${G1} ${prefix}join
+${G1} ${prefix}setpresence
+${G1} ${prefix}setppbot
+${G1} ${prefix}exif
+${G1} ${prefix}addrespon
+${G1} ${prefix}delrespon
+${G1} ${prefix}listrespon
+${G1} ${prefix}online
+${G1} ${prefix}offline
+
+   ${b}🌹【 GROUP MENU 】🌹${b}
+${G1} ${prefix}add
+${G1} ${prefix}kick
+${G1} ${prefix}promote
+${G1} ${prefix}demote
+${G1} ${prefix}inspect
+${G1} ${prefix}setppgroup
+${G1} ${prefix}hidetag
+${G1} ${prefix}tagall
+${G1} ${prefix}welcome
+${G1} ${prefix}antilink
+${G1} ${prefix}totag
+${G1} ${prefix}linkgroup
+${G1} ${prefix}sider
+${G1} ${prefix}opengc
+${G1} ${prefix}closegc
+${G1} ${prefix}groupinfo
+${G1} ${prefix}grouplist
+${G1} ${prefix}creategrup
+
+   ${b}🌹【 DOWNLOAD MENU 】🌹${b}
+${G1} ${prefix}google
+${G1} ${prefix}pinterest
+${G1} ${prefix}ytsearch
+${G1} ${prefix}play
+${G1} ${prefix}ytmp3
+${G1} ${prefix}ytmp4
+${G1} ${prefix}tiktok
+${G1} ${prefix}tiktokaudio
+${G1} ${prefix}mediafire
+${G1} ${prefix}instagram
+
+   ${b}🌹【 GAME MENU 】🌹${b}
+${G1} ${prefix}tictactoe
+${G1} ${prefix}gelud
+${G1} ${prefix}slot
+${G1} ${prefix}suit
+
+   ${b}🌹【 NSFW MENU 】🌹${b}
+${G1} ${prefix}waifu
+${G1} ${prefix}loli
+${G1} ${prefix}pussy
+${G1} ${prefix}neko
+${G1} ${prefix}lesbian
+${G1} ${prefix}kuni
+${G1} ${prefix}cumsluts
+${G1} ${prefix}classic
+${G1} ${prefix}boobs
+${G1} ${prefix}anal
+${G1} ${prefix}avatar
+${G1} ${prefix}yuri
+${G1} ${prefix}trap
+${G1} ${prefix}tits
+${G1} ${prefix}kitsune
+${G1} ${prefix}holo
+${G1} ${prefix}keta
+${G1} ${prefix}hentai
+${G1} ${prefix}futanari
+${G1} ${prefix}femdom
+${G1} ${prefix}feet
+${G1} ${prefix}ero
+${G1} ${prefix}kemonomimi
+${G1} ${prefix}spank
+${G1} ${prefix}cumarts
+${G1} ${prefix}bokep
+${G1} ${prefix}bj
+${G1} ${prefix}blowjob
+${G1} ${prefix}girlsolo
+${G1} ${prefix}holoero
+${G1} ${prefix}erofeet
+${G1} ${prefix}erokitsune
+${G1} ${prefix}eroneko
+${G1} ${prefix}eroyuri
+${G1} ${prefix}erokemonomimi
+
+   ${b}🌹【 STICKER MENU 】🌹${b}
+${G1} ${prefix}attp
+${G1} ${prefix}ttp
+${G1} ${prefix}sticker
+${G1} ${prefix}swm
+${G1} ${prefix}szoom
+${G1} ${prefix}gawrgura
+${G1} ${prefix}doge
+${G1} ${prefix}patrick
+${G1} ${prefix}take
+${G1} ${prefix}toimg
+${G1} ${prefix}tomp4
+${G1} ${prefix}tomp3
+
+   ${b}🌹【 STORY MENU 】🌹${b}
+${G1} ${prefix}upswteks
+${G1} ${prefix}upswimage
+${G1} ${prefix}upswvideo
+
+   ${b}🌹【 TOURL MENU 】🌹${b}
+${G1} ${prefix}tourl
+${G1} ${prefix}imagetorul
+${G1} ${prefix}imgtourl
+${G1} ${prefix}audiotourl
+${G1} ${prefix}videotourl
+${G1} ${prefix}stickertourl
+${G1} ${prefix}stikertourl
+${G1} ${prefix}tolink
+
+   ${b}🌹【 OTHERS MENU 】🌹${b}
+${G1} ${prefix}owner
+${G1} ${prefix}rules
+${G1} ${prefix}script
+${G1} ${prefix}runtime
+${G1} ${prefix}speed
+${G1} ${prefix}delete
+${G1} ${prefix}cekapikey
+
+   ${b}🌹【 GACHA MENU 】🌹${b}
+${G1} ${prefix}cecan
+${G1} ${prefix}cogan
+${G1} ${prefix}cecanvietnam
+${G1} ${prefix}cecanrandom
+${G1} ${prefix}hijaber
+${G1} ${prefix}santuy
+${G1} ${prefix}ukhti
+${G1} ${prefix}bocil
+${G1} ${prefix}ukhti
+${G1} ${prefix}ghea
+${G1} ${prefix}rika
+${G1} ${prefix}ukhti
+${G1} ${prefix}cecanmalaysia
+${G1} ${prefix}cecanindonesia
+${G1} ${prefix}cecankorea
+${G1} ${prefix}cecanjapan
+${G1} ${prefix}cecanthailand
+
+   ${b}🌹【 MY STORE 】🌹${b}
+${G2} ${prefix}order
+${G2} ${prefix}Dmml
+${G2} ${prefix}dmff
+${G2} ${prefix}starlight
+${G2} ${prefix}membership
+${G2} ${prefix}linkgrub
+${G2} ${prefix}linkapk store
+
+   ${b}🌹【 PAYMENT 】🌹${b}
+${G3} ${prefix}Gopay
+${G3} ${prefix}Dana
+${G3} ${prefix}Ovo
+${G3} ${prefix}Payment
+
+✗⃝m🚨 ᴀᴛᴛᴇɴᴛɪᴏɴ
+
+-ᴛʜᴀɴᴋs ғᴏʀ ᴀʟʟ ᴍʏ ᴛᴇᴀᴍ ᴀɴᴅ ᴍʏ ғʀɪᴇɴᴅs
+-ᴛʜɪs sᴄʀɪᴘᴛ ɪs ғᴏʀ sʜᴀʟᴇ ɴᴏᴛ ғʀᴇᴇ
+-ɪғ ʏᴏᴜ ғᴏᴜɴᴅ sᴏᴍᴇ ʙᴜɢ ɪ'ᴍ sᴏʀʀʏ ʙᴇᴄᴀᴜsᴇ ɪs sᴛɪʟʟ ᴜɴᴅᴇʀ ᴅᴇᴠᴇʟᴏᴘᴍᴇɴᴛ
+-ғᴏᴜᴍ ʙᴜɢ?? 
+ʏᴏᴜ ᴄᴀɴ ᴄᴏɴᴛᴀᴄᴛ ᴘᴇʀsᴏɴ ᴅᴇᴠᴇʟᴏᴘᴍᴇɴᴛ
+-ᴄᴏᴍɪɴɢ sᴏᴏɴ ғᴏʀ ᴍᴀɴʏ ғᴇᴀᴛᴜʀᴇs  ! 
+------------------------------------------------------------
+✗⃝📌 𝐘𝐎𝐔𝐓𝐔𝐁𝐄: -
+------------------------------------------------------------
+✗⃝📌𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌: instagram.com/CodeXTeam
+------------------------------------------------------------
+✗⃝📌𝐆𝐈𝐓𝐇𝐔𝐁: https://instagram.com/WolCodeX
+`
+}
